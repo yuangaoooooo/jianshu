2bb332fc4defae5289d7224a8742b58659a61e63
gy <1198929918@qq.com>
2018-02-23 01:20:36 +0800

    消息相关页面全部完成

---
+++ pages/m/guanzhu.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <my-header></my-header>
+        <div class="main">
+
+        </div>
+    </div>
+</template>
+<script>
+    import myHeader from '../../components/myHeader.vue'
+    export default {
+        name:'guanzhu',
+        components:{
+            myHeader
+        },
+        data () {
+            return {
+                
+            }
+        },
+
+    }
+</script>
+<style scoped>
+@media (min-width:992px){
+    .main {
+        width: 960px;
+}
+}
+@media (max-width:1080px){
+    .main {
+        width: 750px;
+    }
+}
+</style>
