b16bacc5ed0a26d2a63e8bc3139a12a145827a70
gy <1198929918@qq.com>
2018-02-28 10:49:48 +0800

    换方法写回复功能，剩余聚焦功能

--- components/myComment.vue
+++ components/myComment.vue
@@ -130,7 +130,7 @@
                                     <i class="fa fa-thumbs-o-up"></i>
                                     <span>{{comment.likes_count}}人点赞</span>
                                 </a>
-                                <a href="javascript:;"  @click="showSubCommentForm(index,'top')">
+                                <a href="javascript:;"  @click="showSubCommentForm(index,'top','')">
                                     <i class="fa fa-comment-o"></i>
                                     <span>回复</span>
                                 </a>
@@ -151,14 +151,14 @@
                             </p>
                             <div class="sub-tool-group">
                                 <span>{{subComment.create_at | formatDate}}</span>
-                                <a href="javascript:;"  @click="showSubCommentAtName(index,subComment.id,subComment.user.nick_name)">
+                                <a href="javascript:;"  @click="showSubCommentForm(index,subComment.id,subComment.user.nick_name)">
                                     <i class="fa fa-comment-o"></i>
                                     <span>回复</span>
                                 </a>
                             </div>
                         </div>
                         <div class="more-comment">
-                            <a class="add-comment-btn" href="javascript:;" @click="showSubCommentForm(index,'bottom')">
+                            <a class="add-comment-btn" href="javascript:;" @click="showSubCommentForm(index,'bottom','')">
                                 <i class="fa fa-pencil"></i>
                                 <span>添加新评论</span>
                             </a>
@@ -364,7 +364,7 @@
                 emojiIndex:[],
                 subCommentList:[],
                 commentFormState:[],
-                commentId:'',
+                commentId:[],
             }
         },
         components:{
@@ -391,52 +391,30 @@
             sendData:function(){
                 console.log('发送value值数据给后端');     
             },   
-            showSubCommentForm:function(index,position){
-                Vue.set(this.subCommentList,index,'');
-                this.commentId = '';
-                if(!this.activeIndex.includes(index)){ 
-                    //第一次点击，总是显示
-                    this.activeIndex.push(index);
-                    // 没吃打开情空内容
-                    Vue.set(this.subCommentList,index,'');
-                    //记录当前点击的下标以及位置
-                    this.commentFormState[index] = position;
-                }else if(this.activeIndex.includes(index)){
-                    if(this.commentFormState[index] !== position){
-                        //点的是另外一个
-                        this.commentFormState[index] = position;
-                        //聚焦一下
-                        let num = this.activeIndex.indexOf(index);
-                        this.$refs.content[num].focus();
-                    }else{
-                        //点的是同一个,将它隐藏掉.
-                        this.activeIndex.splice(this.activeIndex.indexOf(index),1)
-                        this.commentFormState[index] = '';
-                    }
-                 }
+            showSubCommentForm:function(index,id,name){
+                let ID = id.toString();
+               if(this.commentId[index] == ID){
+                   //点两次
+                   this.activeIndex.splice(this.activeIndex.indexOf(index),1);
+                   this.commentId[index] = '';
+               }else{
+                   //点一次
+                   //清除表单内容
+                   this.subCommentList[index] = '';
+                   //表情关掉
+                   this.emojiIndex = [];
+                   if(!this.activeIndex.includes(index)){
+                       this.activeIndex.push(index);
+                   }
+                   // 判断用户名是否存在，如果存在添加
+                   if(name != ''){
+                       this.subCommentList[index] = `@${name} `;
+                   }
+                   //存一下上一个回复列表对应点击的按钮
+                   this.commentId[index] = ID;
+               }
             },
-            showSubCommentAtName:function(index,id,name){
-                this.commentFormState = [];
-                if(this.activeIndex.includes(index)){
-                    if(this.commentId == id){
-                        this.activeIndex.splice(this.activeIndex.indexOf(index),1);
-                        this.commentId = '';
-                        Vue.set(this.subCommentList,index,'');
-                    }else{
-                        //聚焦一下
-                        let num = this.activeIndex.indexOf(index);
-                        this.$refs.content[num].focus();
-                        // 表单已经显示了
-                        Vue.set(this.subCommentList,index,'@' + name + '');
-                        //记录一下上一次点过的id值
-                        this.commentId = id;
-                    }   
-                }else{
-                    //表单没显示出来
-                    this.activeIndex.push(index);
-                    Vue.set(this.subCommentList, index,'@' + name + '');
-                }              
-            },
+            
             sendSubCommentData:function(value){
                 let index = this.activeIndex.indexOf(value);
                 this.activeIndex.splice(index,1);
